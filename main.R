d0d59520e0a599bc003a0308b33ae96055d4f43c
cetaam <tam.tranhcmut@hcmut.edu.vn>
2024-07-07 18:55:07 +0700

    Update main.R

--- main.R
+++ main.R
@@ -43,8 +43,30 @@ missing_data_frequency <- missing_data / 2283
 print(missing_data_frequency)
 # ---------------------------
 # Processing missing data
+tamdata <- new_data[,c("Recommended_Customer_Price", "Graphics_Video_Max_Memory")]
+tamdata$Graphics_Video_Max_Memory<-ifelse(tamdata$Graphics_Video_Max_Memory =="256 MB","0.256 GB",tamdata$Graphics_Video_Max_Memory)
+tamdata$Graphics_Video_Max_Memory<-gsub(" GB","",tamdata$Graphics_Video_Max_Memory)
+tamdata$Graphics_Video_Max_Memory<-as.numeric(tamdata$Graphics_Video_Max_Memory)
+tamdata$Recommended_Customer_Price<-gsub("\\$","",tamdata$Recommended_Customer_Price)
+tamdata$Recommended_Customer_Price<-ifelse(tamdata$Recommended_Customer_Price== "N/A",NA,tamdata$Recommended_Customer_Price)
+tamdata$Recommended_Customer_Price <- sapply(tamdata$Recommended_Customer_Price, function(x){
+  sep <- stringr::str_locate(x, "-")[, 1]
+  if(is.na(sep)) {
+    x
+  } else {
+    as.character(round(median(as.integer(stringr::str_sub(x, c(1L, sep+1), c(sep-1, -1L))))))
+  }
+})
+tamdata$Recommended_Customer_Price<-gsub(".00","",tamdata$Recommended_Customer_Price)
+tamdata$Recommended_Customer_Price<-as.numeric(tamdata$Recommended_Customer_Price)
+
+price_medium<-sum(tamdata$Recommended_Customer_Price,na.rm=TRUE)
+price_medium<-price_medium / 1301
+print(price_medium)
+tamdata$Recommended_Customer_Price<-tidyr::replace_na(tamdata$Recommended_Customer_Price, price_medium)
+new_data$Recommended_Customer_Price<-tamdata$Recommended_Customer_Price
+
 
 ### Paste code in here
-
-
+# ---------------------------
 
