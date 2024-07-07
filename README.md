5d3acb5d143710aee76d666fb0b1e4cb4b39951b
KhoaHo <86819208+it-raining@users.noreply.github.com>
2024-07-07 16:05:35 +0700

    Extract data and count miss entries
    Start code in here

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # PnS_Assignment_233
  BTL XSTK HCMUT-K22
+How to run: Right-click on the main.R file and run in RStudio
--- main.R
+++ main.R
@@ -2,7 +2,49 @@
 
 
 # ---------------------------
+# User Function
+  # Check missing data function
+  count_miss_entries <- function(data){
+    #check NA values
+    na_check <- is.na(data)
+    #check NULL values
+    null_check <- sapply(data , is.null )
+    #check empty string or "N/A"
+    empty_na_check <- sapply(data , function(x) x== "" | x == "N/A")
+    #combine all 
+    missing_data_check <- na_check | null_check | empty_na_check
+    
+    return (sum(missing_data_check))
+  }
+# ---------------------------
 # Read data
 data <- read.csv("Data//Intel_CPUs.csv")
-View(data)
 # ---------------------------
+# Extract data
+new_data <- data[ , c("Product_Collection", 
+                      "Bus_Speed",
+                      "Cache",
+                      "Graphics_Max_Dynamic_Frequency",
+                      "Graphics_Video_Max_Memory",
+                      "Max_Memory_Bandwidth",
+                      "Max_Memory_Size",
+                      "nb_of_Cores",
+                      "Processor_Base_Frequency",
+                      "Recommended_Customer_Price",
+                      "Secure_Key",
+                      "TDP")]
+View(new_data)
+# ---------------------------
+# Count missing data on new data label
+missing_data <- sapply(new_data , count_miss_entries)
+  # Print
+print(missing_data)
+missing_data_frequency <- missing_data / 2283
+print(missing_data_frequency)
+# ---------------------------
+# Processing missing data
+
+### Paste code in here
+
+
+
