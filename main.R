421daecd13f128da136004c8fd90ff7db07f62c0
KhoaHo <86819208+it-raining@users.noreply.github.com>
2024-07-29 01:58:31 +0700

    Add func and change data label
    - Data: change  "Instruction_Set" into "Max_nb_of_Memory_Channels"
    - Function:
    + Add 3 ways to CleanData (remove/ fill by same specific/ fill by average value)
    + Convert vector into num and vector with different unit into standard unit
    - Start doing describe spec

--- main.R
+++ main.R
@@ -1,105 +1,177 @@
 # INSTALL PACKAGE
 # install.packages("tidyr")
+# install.packages("dplyr")
+
 # ---------------------------
 # Includes
 library(tidyr)
-
+library(dplyr)
 
 # ---------------------------
 # User Function
-  # Check missing data function
-  count_miss_entries <- function(data){
-    #check NA values
-    na_check <- is.na(data)
-    #check NULL values
-    null_check <- sapply(data , is.null )
-    #check empty string or "N/A"
-    empty_na_check <- sapply(data , function(x) x== "" | x == "N/A")
-    #combine all 
-    missing_data_check <- na_check | null_check | empty_na_check
-    
-    return (sum(missing_data_check))
+# ---------------------------
+### CLEAN FUNCTIONS ###
+# Brief: Remove "empty" rows from the table
+# Arguments: data - table data; column_names - working column
+# e.g: new_data <- CleanData_rm(new_data, Product_Collection)
+CleanData_rm <- function(data, column_name) {
+  data %>% filter(
+    !is.na({{ column_name }}) &
+      {{ column_name }} != "N/A" &
+      {{ column_name }} != ""
+  )
+}
+# Brief: Fill the missing values by same Sample_Vector name
+# Arguments: data - table data; column_names - working column
+# e.g: new_data <- CleanData_f_name(new_data, Cache, Product_Collection)
+CleanData_f_name <- function(data, column_name, sample_column_name) {
+  data %>%
+    # groups the data frame by the Product_Collection column.
+    group_by(sample_column_name) %>%
+    # Replace NA values with the first non-NA value from
+    # the Product_Collection column.
+    mutate({{ column_name }} := ifelse(
+      {{ column_name }} %in% c(NA, "N/A", ""),
+      first({{ column_name }}[
+        !is.na({{ column_name }}) &
+          {{ column_name }} != "N/A" &
+          {{ column_name }} != ""
+      ]),
+      {{ column_name }}
+    ))
+}
+# Brief: Fill the missing values by average values
+# Arguments: data - table data; column_name - working column
+# e.g: new_data <- CleanData_f_avr(new_data, Product_Collection)
+CleanData_f_avr <- function(data, column_name) {
+  data %>%
+    mutate({{ column_name }} := ifelse(
+      {{ column_name }} %in% c(NA, "N/A", ""),
+      mean(as.numeric({{ column_name }}, na.rm = TRUE), na.rm = TRUE),
+      {{ column_name }}
+    ))
+}
+#-----------------
+### CONVERT FUNCTIONS ###
+# Brief: Convert data into number
+# Arguments: x - data to convert
+# e.g: new_data$col_to_convert <- sapply(new_data$col_to_convert, get_num)
+get_num <- function(x) {
+  return(as.numeric(gsub("[^0-9.]", "", x)))
+}
+#-----------------
+# Brief: Convert data into number
+# Arguments: x - data to convert
+# e.g: new_data$col_to_convert <- sapply(new_data$col_to_convert, get_num)
+freq_to_mhz <- function(x) {
+  # Check if the input is numeric (who knows what is it unit)
+  if (is.numeric(x)) {
+    return(x)
   }
-  #-----------------
-  get_num <- function(x) {
-    return (as.numeric(gsub("[^0-9.]", "", x)))
-  }
-  #-----------------
-  Freq_to_MHz <- function(x) {
-    # Check if the input is numeric
-    if (is.numeric(x)) {
-      return(x)
-    }
-    
-    num <- as.numeric(gsub("[^0-9.]", "", x))
-    # Determine the conversion factor based on the unit
-    if (grepl("K", x, ignore.case = TRUE)) {
-      fac <- 1 / 1000  # kHz to MHz
-    } else if (grepl("M", x, ignore.case = TRUE)) {
-      fac <- 1        # MHz to MHz
-    } else if (grepl("G", x, ignore.case = TRUE)) {
-      fac <- 1000     # GHz to MHz
-    } else if (grepl("T", x, ignore.case = TRUE)) {
-      fac <- 1000000  # THz to MHz
-    } else {
-      fac <- 1  
-    }
-        return(num * fac)
-  }
-  
+  num <- as.numeric(gsub("[^0-9.]", "", x))
+  # Determine the conversion factor based on the unit
+  unit <- toupper(sub("[0-9.]", "", x))
+  fac <- switch(unit,
+    K = 1 / 1000,  # kHz to MHz
+    M = 1,         # MHz to MHz
+    G = 1000,      # GHz to MHz
+    T = 1000000,   # THz to MHz
+    1              # Default: MHz
+  )
+  return(num * fac)
+}
+
+#################################
+#       Data Pre-processing
+#################################
 # ---------------------------
 # Read data
 data <- read.csv("Data//Intel_CPUs.csv")
 # ---------------------------
 # Extract data
-new_data <- data[ , c("Product_Collection", 
-                      "Bus_Speed",
-                      "Instruction_Set",
-                      "Cache",
-                      "Graphics_Max_Dynamic_Frequency",
-                      "Graphics_Video_Max_Memory",
-                      "Max_Memory_Bandwidth",
-                      "Max_Memory_Size",
-                      "nb_of_Cores",
-                      "Processor_Base_Frequency",
-                      "Recommended_Customer_Price",
-                      "Secure_Key",
-                      "TDP")]
+new_data <- data[, c(
+  "Product_Collection",
+  "Bus_Speed",
+  "Cache",
+  "Graphics_Max_Dynamic_Frequency",
+  "Graphics_Video_Max_Memory",
+  "Max_Memory_Bandwidth",
+  "Max_nb_of_Memory_Channels",
+  "Max_Memory_Size",
+  "nb_of_Cores",
+  "Processor_Base_Frequency",
+  "Recommended_Customer_Price",
+  "Secure_Key",
+  "TDP"
+)]
 # ---------------------------
+### PROCESSING MISSING DATA ###
 # Count missing data on new data label
-missing_data <- sapply(new_data , count_miss_entries)
-  # Print
-print(missing_data)
-missing_data_frequency <- missing_data / 2283
-print(missing_data_frequency)
+missing_data <- new_data %>%
+  sapply(
+    function(x) {
+      sum(
+        x %>% is.na() | x %>% sapply(is.null) |
+          x %>% sapply(function(x) (x == "" | x == "N/A"))
+      )
+    }
+  ) %>%
+  print()
+# Count missing data frequency
+missing_data_frequency <- missing_data %>%
+  `/`(nrow(new_data)) %>%
+  print()
 # ---------------------------
-# Processing missing data
+
 
 ### Paste code in here
 
+
+
+
+
+
+
+
 # ---------------------------
-### Instruction_Set ###
-  # UNIT: None
-  # Remove vector contain empty Ins row
-  new_data$Instruction_Set[new_data$Instruction_Set == ""] <- NA 
-  new_data <- new_data[complete.cases(new_data$Instruction_Set), ]
-  # Convert into num
-  new_data$Instruction_Set <- sapply(new_data$Instruction_Set, get_num)
+### Max_nb_of_Memory_Channels ###
+# UNIT: None
+new_data <- new_data %>%
+  mutate(
+    Max_nb_of_Memory_Channels = sapply(Max_nb_of_Memory_Channels, get_num)
+  ) # Convert into number
 # ---------------------------
 ### Bus_Speed ###
-  # UNIT: MHz
-  # Transfer per second to MHz
-  tmp <- separate(new_data, col = Bus_Speed, into = c("Bus_Speed", "Speed_Unit", "Bus_Type"), sep = " ", fill = "right") 
+# UNIT: MHz
+# Transfer per second to MHz
+tmp <- separate(new_data,
+  col = Bus_Speed,
+  into = c("Bus_Speed", "Speed_Unit", "Bus_Type"),
+  sep = " ",
+  fill = "right"
+)
+
+new_data$Bus_Speed <- ifelse(tmp$Speed_Unit == "MHz",
+  as.numeric(tmp$Bus_Speed),
+  as.numeric(tmp$Bus_Speed) * 100
   # Maybe improve when I know wtf is tranfer type and how can convert exactly
-  new_data$Bus_Speed <- ifelse(tmp$Speed_Unit == "MHz", 
-                               as.numeric(tmp$Bus_Speed),
-                               as.numeric(tmp$Bus_Speed) * 100)
-  rm(tmp)
+)
+rm(tmp)
 # ---------------------------
 ### Max Memory Bandwidth ###
-  # UNIT: GB/s
-  new_data$Max_Memory_Bandwidth <- as.numeric(gsub("[^0-9.]", "", new_data$Max_Memory_Bandwidth))
-# ---------------------------  
-  
-  
-  
+# UNIT: GB/s
+new_data <- new_data %>%
+  mutate(
+    Max_Memory_Bandwidth = sapply(Max_Memory_Bandwidth, get_num)
+  )
+# ---------------------------
+#################################
+#       Descriptive statistics
+#################################
+# ---------------------------
+### Summary statistics ###
+summary_stats <- new_data %>%
+  summarise_all(funs(mean, median, sd, min, max))
+print(summary_stats)
+# ---------------------------
+### Correlation matrix ###
